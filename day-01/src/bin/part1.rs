933fa4fc6fc7dda43f3fe686fb1e2f0d1328bfc0
Ilya Pashkovskiy <ilya.pashkovskiy@gmail.com>
2023-12-01 06:08:48 +0100

    Preparations for Day 1

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -3,7 +3,7 @@ fn process(input: &str) -> &str {
 }
 
 fn main() {
-    let input = include_str!("../input.txt");
+    let input = include_str!("../../input.txt");
     process(input);
 }
 
@@ -11,7 +11,10 @@ fn main() {
 mod tests {
     use super::*;
 
-    const TEST: &str = "";
+    const TEST: &str = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
 
     #[test]
     fn it_works() {
