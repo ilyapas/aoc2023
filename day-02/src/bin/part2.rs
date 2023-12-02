276f82d465e7d2fffc1cdcc838e9ca23afb3d5eb
Ilya Pashkovskiy <ilya.pashkovskiy@gmail.com>
2023-12-02 07:11:37 +0100

    Solve Day 2

---
+++ day-02/src/bin/part2.rs
@@ -0,0 +1,86 @@
+use std::cmp::max;
+
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{digit1, line_ending},
+    combinator::map_res,
+    multi::separated_list1,
+    IResult,
+};
+
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
+
+fn color(input: &str) -> IResult<&str, (Color, u32)> {
+    let (input, number) = map_res(digit1, |s: &str| s.parse::<u32>())(input)?;
+    let (input, _) = tag(" ")(input)?;
+    let (input, color) = map_res(
+        alt((tag("red"), tag("green"), tag("blue"))),
+        |s: &str| match s {
+            "red" => Ok(Color::Red),
+            "green" => Ok(Color::Green),
+            "blue" => Ok(Color::Blue),
+            _ => Err(()),
+        },
+    )(input)?;
+    Ok((input, (color, number)))
+}
+
+fn round(input: &str) -> IResult<&str, (u32, u32, u32)> {
+    let (input, colors) = separated_list1(tag(", "), color)(input)?;
+    let mut red = 0;
+    let mut green = 0;
+    let mut blue = 0;
+    colors.iter().for_each(|(c, n)| match c {
+        Color::Red => red += n,
+        Color::Green => green += n,
+        Color::Blue => blue += n,
+    });
+    Ok((input, (red, green, blue)))
+}
+
+fn game(input: &str) -> IResult<&str, Vec<(u32, u32, u32)>> {
+    let (input, _) = tag("Game ")(input)?;
+    let (input, _) = digit1(input)?;
+    let (input, _) = tag(": ")(input)?;
+    let (input, rounds) = separated_list1(tag("; "), round)(input)?;
+    Ok((input, rounds))
+}
+
+fn process(input: &str) -> u32 {
+    let (_, games) = separated_list1(line_ending, game)(input).unwrap();
+    games
+        .iter()
+        .map(|game| {
+            game.iter().fold((0u32, 0u32, 0u32), |acc, (r, g, b)| {
+                (max(acc.0, *r), max(acc.1, *g), max(acc.2, *b))
+            })
+        })
+        .map(|(r, g, b)| r * g * b)
+        .sum()
+}
+
+fn main() {
+    let input = include_str!("../../input.txt");
+    dbg!(process(input));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST: &str = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+
+    #[test]
+    fn it_works() {
+        assert_eq!(process(TEST), 2286);
+    }
+}
