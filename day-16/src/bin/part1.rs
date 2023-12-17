e65ccdeff94339b089171d158e7bc1c48d0ca4e3
Ilya Pashkovskiy <ilya.pashkovskiy@gmail.com>
2023-12-17 19:48:06 +0100

    Refactor Day 16

--- day-16/src/bin/part1.rs
+++ day-16/src/bin/part1.rs
@@ -1,143 +1,64 @@
 use std::collections::{BTreeMap, BTreeSet, VecDeque};
 
 fn process(input: &str) -> usize {
-    let mut redirects: BTreeMap<((isize, isize), (isize, isize)), Vec<(isize, isize)>> =
-        BTreeMap::new();
     let mut grid: BTreeMap<(isize, isize), char> = BTreeMap::new();
     for (y, line) in input.lines().enumerate() {
         for (x, c) in line.chars().enumerate() {
-            let x = x as isize;
-            let y = y as isize;
-            grid.insert((x, y), c);
-            match c {
-                '|' => {
-                    redirects
-                        .entry(((x - 1, y), (x, y)))
-                        .or_default()
-                        .push((x, y - 1));
-                    redirects
-                        .entry(((x - 1, y), (x, y)))
-                        .or_default()
-                        .push((x, y + 1));
-                    redirects
-                        .entry(((x + 1, y), (x, y)))
-                        .or_default()
-                        .push((x, y - 1));
-                    redirects
-                        .entry(((x + 1, y), (x, y)))
-                        .or_default()
-                        .push((x, y + 1));
-                    redirects
-                        .entry(((x, y - 1), (x, y)))
-                        .or_default()
-                        .push((x, y + 1));
-                    redirects
-                        .entry(((x, y + 1), (x, y)))
-                        .or_default()
-                        .push((x, y - 1));
-                }
-                '-' => {
-                    redirects
-                        .entry(((x, y - 1), (x, y)))
-                        .or_default()
-                        .push((x - 1, y));
-                    redirects
-                        .entry(((x, y - 1), (x, y)))
-                        .or_default()
-                        .push((x + 1, y));
-                    redirects
-                        .entry(((x, y + 1), (x, y)))
-                        .or_default()
-                        .push((x - 1, y));
-                    redirects
-                        .entry(((x, y + 1), (x, y)))
-                        .or_default()
-                        .push((x + 1, y));
-                    redirects
-                        .entry(((x - 1, y), (x, y)))
-                        .or_default()
-                        .push((x + 1, y));
-                    redirects
-                        .entry(((x + 1, y), (x, y)))
-                        .or_default()
-                        .push((x - 1, y));
-                }
-                '/' => {
-                    redirects
-                        .entry(((x - 1, y), (x, y)))
-                        .or_default()
-                        .push((x, y - 1));
-                    redirects
-                        .entry(((x + 1, y), (x, y)))
-                        .or_default()
-                        .push((x, y + 1));
-                    redirects
-                        .entry(((x, y - 1), (x, y)))
-                        .or_default()
-                        .push((x - 1, y));
-                    redirects
-                        .entry(((x, y + 1), (x, y)))
-                        .or_default()
-                        .push((x + 1, y));
-                }
-                '\\' => {
-                    redirects
-                        .entry(((x - 1, y), (x, y)))
-                        .or_default()
-                        .push((x, y + 1));
-                    redirects
-                        .entry(((x + 1, y), (x, y)))
-                        .or_default()
-                        .push((x, y - 1));
-                    redirects
-                        .entry(((x, y - 1), (x, y)))
-                        .or_default()
-                        .push((x + 1, y));
-                    redirects
-                        .entry(((x, y + 1), (x, y)))
-                        .or_default()
-                        .push((x - 1, y));
-                }
-                _ => {
-                    redirects
-                        .entry(((x - 1, y), (x, y)))
-                        .or_default()
-                        .push((x + 1, y));
-                    redirects
-                        .entry(((x + 1, y), (x, y)))
-                        .or_default()
-                        .push((x - 1, y));
-                    redirects
-                        .entry(((x, y - 1), (x, y)))
-                        .or_default()
-                        .push((x, y + 1));
-                    redirects
-                        .entry(((x, y + 1), (x, y)))
-                        .or_default()
-                        .push((x, y - 1));
-                }
-            }
+            grid.insert((x as isize, y as isize), c);
         }
     }
 
-    let mut queue: VecDeque<((isize, isize), (isize, isize))> = VecDeque::new();
-    let mut visited: BTreeSet<((isize, isize), (isize, isize))> = BTreeSet::new();
+    let width = input.lines().next().unwrap().len() as isize;
+    let height = input.lines().count() as isize;
+    let mut queue: VecDeque<(isize, isize, isize, isize)> = VecDeque::new();
+    let mut visited: BTreeSet<(isize, isize, isize, isize)> = BTreeSet::new();
     let mut energized: BTreeSet<(isize, isize)> = BTreeSet::new();
-    queue.push_back(((-1, 0), (0, 0)));
+    queue.push_back((0, 0, 1, 0));
     while queue.len() > 0 {
-        let (prev, current) = queue.pop_front().unwrap();
-        if visited.contains(&(prev, current)) {
+        let (x, y, dx, dy) = queue.pop_front().unwrap();
+        if visited.contains(&(x, y, dx, dy)) {
             continue;
         }
-        visited.insert((prev, current));
-        if let Some(redirects) = redirects.get(&(prev, current)) {
-            energized.insert(current);
-            for redirect in redirects {
-                if visited.contains(&(current, *redirect)) {
-                    continue;
+        if x < 0 || x >= width || y < 0 || y >= height {
+            continue;
+        }
+        visited.insert((x, y, dx, dy));
+        energized.insert((x, y));
+        match grid.get(&(x, y)) {
+            Some('|') => {
+                if dx == 0 {
+                    queue.push_back((x, y + dy, 0, dy));
+                } else {
+                    queue.push_back((x, y - 1, 0, -1));
+                    queue.push_back((x, y + 1, 0, 1));
                 }
-                queue.push_back((current, *redirect));
             }
+            Some('-') => {
+                if dy == 0 {
+                    queue.push_back((x + dx, y, dx, 0));
+                } else {
+                    queue.push_back((x - 1, y, -1, 0));
+                    queue.push_back((x + 1, y, 1, 0));
+                }
+            }
+            Some('/') => {
+                if dx == 0 {
+                    queue.push_back((x - dy, y, -dy, 0));
+                } else {
+                    queue.push_back((x, y - dx, 0, -dx));
+                }
+            }
+            Some('\\') => {
+                if dx == 0 {
+                    queue.push_back((x + dy, y, dy, 0));
+                } else {
+                    queue.push_back((x, y + dx, 0, dx));
+                }
+            }
+            Some('.') => {
+                queue.push_back((x + dx, y + dy, dx, dy));
+            }
+            _ => {}
         }
     }
     energized.len()
